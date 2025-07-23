9d89dfc29c6876f9c4495e4efb5e42f2208c3936
smashtitle <smashtitle@fastmail.com>
2025-07-22 20:02:54 -0700

    Update setup_workstation.ps1

--- setup_workstation.ps1
+++ setup_workstation.ps1
@@ -4,4 +4,6 @@ powershell -ExecutionPolicy Bypass -File "$tempPath`ConfigureWinRM.ps1"
 
 iwr -Uri "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-9.0.3-windows-x86_64.zip" -OutFile "$tempPath`winlogbeat-9.0.3-windows-x86_64.zip"
 Expand-Archive -Path "$tempPath`winlogbeat-9.0.3-windows-x86_64.zip" -DestinationPath "C:\\Tools"
-Set-Content -Path ""$tempPath`winlogbeat-9.0.3-windows-x86_64.zip" 
+
+iwr -Uri "https://raw.githubusercontent.com/smashtitle/TelForge/main/winlogbeat.yml" -OutFile "$tempPath`winlogbeat.yml"
+Copy-Item -Path "$tempPath`winlogbeat.yml" -Destination "C:\\Tools\\winlogbeat-9.0.3-windows-x86_64\winlogbeat.yml" -Force
