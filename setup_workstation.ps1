3569a0e779ea84a11e5c182ba948ec90ec767062
smashtitle <smashtitle@fastmail.com>
2025-08-07 19:32:22 -0700

    Update setup_workstation.ps1

--- setup_workstation.ps1
+++ setup_workstation.ps1
@@ -13,7 +13,7 @@ try {
     Write-Host "C:\Tools has been excluded from Defender scans."
 
     Write-Host "Applying event log baseline settings..."
-    $baselineBatUri = "https://raw.githubusercontent.com/smashtitle/TelForge/main/ASD-Servers.bat"
+    $baselineBatUri = "https://raw.githubusercontent.com/smashtitle/TelForge/refs/heads/main/ASD-Servers.bat"
     $baselineBatPath = Join-Path $tempDir "ASD-Servers.bat"
     Invoke-WebRequest -Uri $baselineBatUri -OutFile $baselineBatPath
     Start-Process -FilePath $baselineBatPath -Wait
@@ -25,7 +25,7 @@ try {
     $sysmonZipPath = Join-Path $tempDir "Sysmon.zip"
     Invoke-WebRequest -Uri $sysmonZipUri -OutFile $sysmonZipPath
     Expand-Archive -Path $sysmonZipPath -DestinationPath $sysmonDir -Force
-    $sysmonConfigUri = "https://raw.githubusercontent.com/smashtitle/TelForge/main/sysmonconfig-research.xml"
+    $sysmonConfigUri = "https://raw.githubusercontent.com/smashtitle/TelForge/refs/heads/main/sysmonconfig-research.xml"
     $sysmonConfigXml = Join-Path $sysmonDir "sysmonconfig-research.xml"
     Invoke-WebRequest -Uri $sysmonConfigUri -OutFile $sysmonConfigXml
     $sysmonExe = Join-Path $sysmonDir "Sysmon64.exe"
